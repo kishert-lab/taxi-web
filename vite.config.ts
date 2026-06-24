a2b6fc8a351f4b396a3ad52cad90f41f17fcec1c
Sergio PC-HOME <it59com@mail.ru>
2026-06-24 20:08:00 +0500

    drom proxy config

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://192.168.0.50:8080',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api/v1')
-      },
-      '/ws': {
-        target: 'ws://192.168.0.50:8080',
-        ws: true,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/ws/, '/api/v1/ws')
-      }
-    }
+  
   }
 })
